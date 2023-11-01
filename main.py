f811b011aaa02f31941804de59ae7c5678fcfa2a
chiagoziem <52287428+chiagoziem-code@users.noreply.github.com>
2023-11-01 18:08:58 +0100

    Add files via upload
    Was trying something new

---
+++ main.py
@@ -0,0 +1,34 @@
+#This is a practice exercise
+
+#collecting database
+
+name = "chiagoziem"
+age = 21
+Hobbies = ["games", "ideating"]
+color = "black"
+answer = "yes"
+
+#what to play
+
+print("Hello",name)
+
+print()
+
+print ("I hope you are happy today!!")
+if answer == "yes":
+   print("That is great to hear!!")
+else: 
+   print("that is sad to hear \nI hope you are better!!")
+
+print("what is your best colour?")
+if age < 21:
+   print("Ouch \nYou are not eligible")
+else: 
+   print("Yay, Welcome to the team!!")
+
+print()
+print("What is your favourite color")
+if color == "black": 
+   print("Black beauty")
+else :
+   print("eww")
