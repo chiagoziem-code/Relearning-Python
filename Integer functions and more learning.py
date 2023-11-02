2f71d8274ed5fd6969f92da3ca4ff9dc2d00f2ba
chiagoziem <52287428+chiagoziem-code@users.noreply.github.com>
2023-11-03 00:51:52 +0100

    Updated learning on Integers and floating numbers
    I learned the difference between integers, hexadecimals and how to represnt octa and floating numbers, Yayyyy

---
+++ Integer functions and more learning.py
@@ -0,0 +1,18 @@
+#trying out integer functions like octa and hexa decimal
+
+#For octa
+print(0o1)
+print(0o150)
+
+#For hexadecimal
+print(0x100)
+
+#Understanding the difference between integer and floating numbers
+
+#For floating numbers
+print(0.65)
+print(-2.5)
+
+#For integers
+print(23)
+print(500)
